6bf70f184b190fbc844ffdd91d93d87be3a0263d
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-21 22:21:25 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if  self.end_with?(".")
+    return true
+   else return false
+   end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else return false
+    end
   end
 
   def count_sentences
+    count = 0
+    self.split.each do |i|
+        #binding.pry
+        if i.end_with?(".", "?", "!") == true
+          count += 1
 
+  #end_with(".")
+        end
+      end
+      return count
   end
 end
